dc2f8a3c950e3047e9747f5fd93937d74be33444
Melih Sami Özaydın <30636062+melihozaydin@users.noreply.github.com>
2022-02-20 14:17:07 +0300

    bed offset x

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1385,8 +1385,8 @@
 #define X_MIN_POS 10
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS X_BED_SIZE + X_MIN_POS
+#define Y_MAX_POS Y_BED_SIZE + Y_MIN_POS
 #define Z_MAX_POS 210
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50

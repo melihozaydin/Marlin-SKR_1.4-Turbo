761d2bc60c6056cc71f7a30eec68a967bbc7cf79
Melih Sami Özaydın <30636062+melihozaydin@users.noreply.github.com>
2022-03-13 16:21:30 +0300

    offset test

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1382,12 +1382,12 @@
 #define Y_BED_SIZE 210
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS 13
+#define Y_MIN_POS 6
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE + X_MIN_POS
 #define Y_MAX_POS Y_BED_SIZE + Y_MIN_POS
-#define Z_MAX_POS 210
+#define Z_MAX_POS 200
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
